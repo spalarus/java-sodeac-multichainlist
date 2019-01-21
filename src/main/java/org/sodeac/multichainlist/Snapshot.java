8be9c77a04dc660ea6003ec1d1c7e62d76be5d2e
spalarus <s.palarus@googlemail.com>
2019-01-21 20:51:47 +0100

    fix header

--- src/main/java/org/sodeac/multichainlist/Snapshot.java
+++ src/main/java/org/sodeac/multichainlist/Snapshot.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Sebastian Palarus
+ * Copyright (c) 2018, 2019 Sebastian Palarus
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
